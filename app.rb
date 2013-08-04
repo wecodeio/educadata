a614ea656c965c187421a7eaa702b163f3c63a36
Nicolás Cerrini <ceneon@gmail.com>
2013-08-03 21:02:11 -0300

    Added simple expenses graph

--- app.rb
+++ app.rb
@@ -1,10 +1,21 @@
 require 'sinatra'
 require_relative 'config/initializers/sequel'
+require_relative 'config/initializers/sequel'
 
 get '/' do 
 	erb :index
 end
 
 get '/inversion_en_educacion' do 
-	erb :inversion
+	json = "[
+          ['Year', 'Sales', 'Expenses'],
+          ['2004',  1000,      400],
+          ['2005',  1170,      460],
+          ['2006',  660,       1120],
+          ['2007',  1030,      540]
+        ]"
+    #json = "['Año', '% PBI'] ,"
+	#json += "['2009', '5.3'], ['2010', '5.3'], ['2011', '5.3']"
+	erb :inversion, :locals => { :json => json }
 end
+
